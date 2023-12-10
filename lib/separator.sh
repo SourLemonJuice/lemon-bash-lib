ec2f8702bf4cf6b3630ee4832aa6b19437fa9b95
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2023-12-11 01:50:58 +0800

    为分割线函数添加一些注释

--- lib/separator.sh
+++ lib/separator.sh
@@ -1,11 +1,13 @@
 # 分隔线函数
+# 参数:
+# $1 == _Character(default == '=')
 llib_separator(){
     if [[ -z $1 ]] || [[ ! ${#1} -eq 1 ]]; then
-        Character='='
+        _Character='='
     else
-        Character=$1
+        _Character=$1
     fi
 
     ShellWidth=$(stty size|awk '{print $2}')
-    yes "$Character" |sed $ShellWidth'q' |tr -d "\n" && echo 
+    yes "$_Character" |sed $ShellWidth'q' |tr -d "\n" && echo 
 }
