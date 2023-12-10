6b6cc931e03a56fc39c6774c02def23a57228a6d
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2023-12-11 05:26:15 +0800

    删除注释函数添加单用命令本身就能输出到文件的选项

--- lib/remove_script_comment.sh
+++ lib/remove_script_comment.sh
@@ -1,6 +1,13 @@
 # remove_script_comment
-# use: input file in $1,script can print it
+# use:
+# 1. input file in $1,script can echo them.
+# 2. if have $2 , then output to file($2).
 # it's will remove all line starting with '#' incloud shebang(#!/bin/sh)
 llib-remove_script_comment(){
-    grep --invert-match '^[[:blank:]]*#' $1
+    # 如果 $2 不为空
+    if [[ ! -z $2 ]];then
+        grep --invert-match '^[[:blank:]]*#' $1 > $2
+    else
+        grep --invert-match '^[[:blank:]]*#' $1
+    fi
 }
