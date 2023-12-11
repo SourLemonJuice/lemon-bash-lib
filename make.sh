26c3cf3a41edd7d52eca367a3b2051355bf48635
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2023-12-11 19:30:48 +0800

    储存残废的make.sh,对不起make，是我的错

--- make.sh
+++ make.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 # make is not good at handling script,only script can fight script.(just lazy)
 # sorry,make先輩
+# don't use this script,i was wrong.
 _DIR=./lib
 _OUTPUT=./target
 source $_DIR/merge_function.sh
+source $_DIR/remove_script_comment.sh
 [[ -d target ]] || mkdir target
 
+build-error_exit(){
+    _OUTNAME=errored_exit.sh
+    llib-remove_script_comment $_DIR/$_OUTNAME $_OUTPUT/$_OUTNAME
+}
 build-read_config-bash(){
     _OUTNAME=read_config-bash.sh
-    llib_merge_function $_OUTPUT/$_OUTNAME $_DIR/errored_exit.sh
+    llib_merge_function $_OUTPUT/$_OUTNAME $_OUTPUT/errored_exit.sh
     llib_merge_function $_OUTPUT/$_OUTNAME $_DIR/read_config-bash.sh
 }
 build-prompts_need_perm(){
     _OUTNAME=read_config-bash.sh
-    llib_merge_function $_OUTPUT/$_OUTNAME $_DIR/errored_exit.sh
+    llib_merge_function $_OUTPUT/$_OUTNAME $_OUTPUT/errored_exit.sh
     llib_merge_function $_OUTPUT/$_OUTNAME $_DIR/prompts_need_perm.sh
 }
 
 case $1 in
-    all)
-        build-read_config-bash
-        build-prompts_need_perm
+    error_exit)
+        build-error_exit
     ;;
     read_config-bash)
+        build-error_exit
         build-read_config-bash
+    ;;
+    prompts_need_perm)
+        build-error_exit
+        build-prompts_need_perm
     ;;
     clear)
         rm $_OUTPUT/*
