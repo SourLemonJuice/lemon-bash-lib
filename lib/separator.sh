cb219e61dd17e67a9685e0d2375a247b855ed397
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2024-01-02 12:03:09 +0800

    完善 separator.sh - 允许用 相对于终端宽度的方式 控制输出的宽度 - 完善注释 变更的版本号: separator.sh -> v2

--- lib/separator.sh
+++ lib/separator.sh
@@ -1,6 +1,12 @@
+# separator v2
 # 分隔线函数
-# 参数:
-# $1 == _Character(default == '=')
+# options:
+# $1 (optional,default == '=')
+#   option's value == $_Character
+# 
+# $2 (optional,default ≈ '1')
+#   it's used for control $_Separator_Width
+#   expression: $[$_Shell_Width/$2]
 llib_separator(){
     if [[ -z $1 ]] || [[ ! ${#1} -eq 1 ]]; then
         _Character='='
@@ -8,6 +14,15 @@ llib_separator(){
         _Character=$1
     fi
 
-    ShellWidth=$(stty size|awk '{print $2}')
-    yes "$_Character" |sed $ShellWidth'q' |tr -d "\n" && echo 
+    # 获取终端宽度
+    _Shell_Width=$(stty size|awk '{print $2}')
+    # 用输入的信息计算出要打出多长的分割线
+    if [[ -z $2 ]]; then
+        # 如果没有输入则直接用终端宽度
+        _Separator_Width=$_Shell_Width
+    else
+        _Separator_Width=$[$_Shell_Width/$2]
+    fi
+    # 输出
+    yes "$_Character" |sed $_Separator_Width'q' |tr -d "\n" && echo 
 }
