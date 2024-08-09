2a7f07f6886eaf302dfcb07d0f5db3f0e8563e43
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-09 12:14:25 +0800

    更新 clipboard.sh
    现在可以在 xorg 下获取剪贴板咯

--- lib/clipboard.sh
+++ lib/clipboard.sh
@@ -1,9 +1,9 @@
-# clipboard v2
+# lemon-bash-lib | clipboard | v3
 # dependency:
 # - wl-clipboard
 # - xclip
 
-llib_clipboard_copy() {
+clipboard_copy() {
     # 检测正在使用的桌面gui程序，并选择对应的剪贴板工具
     case $XDG_SESSION_TYPE in
     wayland)
@@ -12,21 +12,21 @@ llib_clipboard_copy() {
         ;;
     x11)
         # 那这个就是老古董x11的喽
-        # xclip 的实现
-        echo "$1" | xclip -selection c
-        # xsel 的实现
+        # 使用 xclip
+        echo "$1" | xclip -selection clipboard -in
+        # 使用 xsel
         # echo "$Parsed_Code" | xsel -ib
         ;;
     esac
 }
 
-llib_clipboard_paste() {
+clipboard_paste() {
     case $XDG_SESSION_TYPE in
     wayland)
         wl-paste
         ;;
     x11)
-        echo "只找了wayland模式的工具嘞"
+        xclip -selection clipboard -out
         ;;
     esac
 }
