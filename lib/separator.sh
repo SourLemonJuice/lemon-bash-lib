71b8bfdf7d6ddda14495d0ed3d0cf716f4ab9bf5
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-11-04 20:53:58 +0800

    更新 separator.sh，细节修缮且没有参数改动

--- lib/separator.sh
+++ lib/separator.sh
@@ -1,28 +1,30 @@
-# separator v2
+# lemon-bash-lib | separator | v3
 # 分隔线函数
 # options:
 # $1 (optional,default == '=')
-#   option's value == $_Character
-# 
-# $2 (optional,default ≈ '1')
-#   it's used for control $_Separator_Width
-#   expression: $[$_Shell_Width/$2]
-llib_separator(){
+#   option's value == $display_char
+#
+# $2 (optional,default similar to '1')
+#   it's used for control $spearator_width
+#   expression: $[$shell_width/$2]
+function separator {
+    local display_char
     if [[ -z $1 ]] || [[ ! ${#1} -eq 1 ]]; then
-        _Character='='
+        display_char='='
     else
-        _Character=$1
+        display_char=$1
     fi
 
     # 获取终端宽度
-    _Shell_Width=$(stty size|awk '{print $2}')
+    local shell_width=$(stty size | awk '{print $2}')
     # 用输入的信息计算出要打出多长的分割线
+    local spearator_width
     if [[ -z $2 ]]; then
         # 如果没有输入则直接用终端宽度
-        _Separator_Width=$_Shell_Width
+        spearator_width=$shell_width
     else
-        _Separator_Width=$[$_Shell_Width/$2]
+        spearator_width=$(($shell_width / $2))
     fi
     # 输出
-    yes "$_Character" |sed $_Separator_Width'q' |tr -d "\n" && echo 
+    yes "$display_char" | sed "${spearator_width}q" | tr -d "\n" && echo ""
 }
